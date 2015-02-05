43674ce6e36073c7d26631791ec73adb44de0b52
krisziel <krisziel@mac.com>
2015-02-05 12:21:12 -0800

    change to integer

--- db/migrate/20150205201607_change_uaid_to_integer.rb
+++ db/migrate/20150205201607_change_uaid_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeUaidToInteger < ActiveRecord::Migration
   def change
-    change_column :flights, :user_aircraft_id, :integer
+    change_column :flights, :user_aircraft_id, 'integer USING CAST(user_aircraft_id AS integer)'
   end
 end
